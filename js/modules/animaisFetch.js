8eeb8c46830d390466fa7bbff1094877dbab9d71
Bruno Leonardo <bruno@mktouch.com.br>
2022-02-16 10:59:11 -0300

    Update Animals Numbers

--- js/modules/animaisFetch.js
+++ js/modules/animaisFetch.js
@@ -1,6 +1,7 @@
 import AnimaNumeros from "./animaNumeros.js";
 
 export default function initAnimaisFetch() {
+
   async function animaisFetch(url) {
     try {
       const animaisResponse = await fetch(url);
@@ -10,8 +11,8 @@ export default function initAnimaisFetch() {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-      const animaNumeros = new AnimaNumeros("[data-numero]", "numeros", "ativo");
-      animaNumeros();
+      const animaNumeros = new AnimaNumeros("[data-numero]", ".numeros", "ativo");
+      animaNumeros.init();
     } catch (error) {
       console.log(error);
     }
